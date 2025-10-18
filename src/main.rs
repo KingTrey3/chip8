29c24cf91a49f1ca4147b36bd8f14aa1d9b0c4b2
trey03 <adixon1@unc.edu>
2025-10-17 21:56:52 -0400

    Basic outline of emulator and ret

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::array;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+struct CPU {
+    v: [u8; 16],
+    delay: u8,
+    sound: u8,
+    program_counter: u16,
+    stack_pointer: u8,
+    stack: [u16; 16]
+}
+
+impl CPU {
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("RET called on empty stack")
+        } else {
+            self.stack_pointer -= 1;
+            self.program_counter = self.stack[self.stack_pointer as usize];
+        }
+    }
+}
+
+struct keyboard {
+    keys: [bool; 16]
+}
+
+struct Chip8 {
+    memory: [u8; 4096],
+    cpu: CPU,
+    keyboard: keyboard,
+    // display: display,
+    // timers: timers,
+    // sound: sound
+}
+
